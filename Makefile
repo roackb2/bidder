9353796463c0384e25a57e1d02dc6f2fd4e6df4c
Jay Liang <roackb2@gmail.com>
2016-07-03 10:38:29 +0800

    established connection to Redis and RabbitMQ

--- Makefile
+++ Makefile
@@ -2,10 +2,18 @@ project_name = bidder
 base_dockerfile = build/base.Dockerfile
 server_dockerfile = build/prod.Dockerfile
 simple_base_image_tag = node-base
-registry = $(shell echo $$AWS_REGISTRY)
 base_image_tag = $(registry):$(simple_base_image_tag)
 container_port = 3000
 start_cmd = node app.js
+
+aws_access_key_id = $(shell echo $$AWS_ACCESS_KEY_ID)
+aws_secret_access_key = $(shell echo $$AWS_SECRET_ACCESS_KEY)
+registry = $(shell echo $$AWS_REGISTRY)
+redis_cluster = $(shell echo $$REDIS_CLUSTER)
+rabbit_host = $(shell echo $$RABBIT_HOST)
+rabbit_user = $(shell echo $$RABBIT_USER)
+rabbit_password = $(shell echo $$RABBIT_PASSWORD)
+rabbit_vhost = $(shell echo $$RABBIT_VHOST)
 
 # build the base image
 build-base: $(base_dockerfile)
@@ -21,6 +29,13 @@ run-server:
 	docker run -it --rm \
 	--name $(project_name) \
 	-p 80:$(container_port) \
+	-e "AWS_ACCESS_KEY_ID=$(aws_access_key_id)" \
+	-e "AWS_SECRET_ACCESS_KEY=$(aws_secret_access_key)" \
+	-e "REDIS_CLUSTER=$(redis_cluster)" \
+	-e "RABBIT_HOST=$(rabbit_host)" \
+	-e "RABBIT_USER=$(rabbit_user)" \
+	-e "RABBIT_PASSWORD=$(RABBIT_PASSWORD)" \
+	-e "RABBIT_VHOST=$(rabbit_vhost)" \
 	$(project_name):latest
 
 build-and-run-server: build-server run-server
--- app.js
+++ app.js
@@ -7,15 +7,99 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bluebird = require('bluebird')
+var redis = require('redis');
+
+// handling signals if process is PID1 in containers
+var signals = {
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+
+function shutdown(signal, value) {
+    io.close(function() {
+        console.log('server stopped by ' + signal);
+        process.exit(128 + value);
+    });
+}
+
+Object.keys(signals).forEach(function(signal) {
+    process.on(signal, function() {
+        shutdown(signal, signals[signal]);
+    });
+});
+
+// setup socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('start message', function(msg) {
+        console.log('user send start message: ' + msg)
+    })
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+// setup redis
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+var redisClient = redis.createClient('6379', process.env.REDIS_CLUSTER);
+
+redisClient.on("ready", function() {
+    console.log("redies ready")
+})
+
+redisClient.on("error", function(error) {
+    console.log(error);
+});
+
+// setup RabbitMQ
+var amqp = require('amqp');
+var rabbitConn = amqp.createConnection({
+    host: process.env.RABBIT_HOST,
+    port: 5672,
+    login: process.env.RABBIT_USER,
+    password: process.env.RABBIT_PASSWORD,
+    connectionTimeout: 10000,
+    authMechanism: 'AMQPLAIN',
+    vhost: process.env.RABBIT_VHOST,
+    noDelay: true,
+    ssl: {
+        enabled: false
+    }
+});
+
+rabbitConn.on('error', function(e) {
+    console.log("Error from amqp: ", e);
+});
+
+// Wait for connection to become established.
+rabbitConn.on('ready', function() {
+    console.log("rabbit ready")
+        // // Use the default 'amq.topic' exchange
+        // rabbitConn.queue('my-queue', function (q) {
+        //     // Catch all messages
+        //     q.bind('#');
+        //
+        //     // Receive messages
+        //     q.subscribe(function (message) {
+        //       // Print messages to stdout
+        //       console.log(message);
+        //     });
+        // });
+});
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -61,44 +145,6 @@ app.use(function(err, req, res, next) {
 
 // module.exports = app;
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-// app.get('/', function(req, res){
-//   res.sendfile('index.html');
-// });
-
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('start message', function(msg) {
-        console.log('user send start message: ' + msg)
-    })
-
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-});
-
 http.listen(3000, function() {
-    console.log('listening on *:3000');
-});
-
-
-// handling signals if process is PID1 in containers
-var signals = {
-    'SIGINT': 2,
-    'SIGTERM': 15
-};
-
-function shutdown(signal, value) {
-    io.close(function() {
-        console.log('server stopped by ' + signal);
-        process.exit(128 + value);
-    });
-}
-
-Object.keys(signals).forEach(function(signal) {
-    process.on(signal, function() {
-        shutdown(signal, signals[signal]);
-    });
+    console.log('server listening on *:3000');
 });
